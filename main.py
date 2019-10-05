5df58ca0fe66387ebc919c578c142bf917cf937b
jeremyzhangsq <jeremyzhangsq@gmail.com>
2019-10-05 15:32:29 +0800

    add evaluation matrics

--- main.py
+++ main.py
@@ -9,19 +9,22 @@ from NMF import NMF
 
 if __name__ == '__main__':
 
+    # input params
     ufile = sys.argv[1] # network filename
     cfile = sys.argv[2] # ground truth community filename
     algorithm = sys.argv[3] # select an algorithm. option: bigclam, nmf, lc, cpm
     k = int(sys.argv[4]) # number of community to detect
     output = sys.argv[5] # output filename
 
-    matrics = dict()
+    metrics = dict()
 
+    # load two matrix
     start = time.time()
     graph = Util.readNetwork(ufile) # return a n*n matrix
     realComm = Util.readCommunity(cfile) # return a n*k matrix
-    matrics["readTime"] = time.time()-start
+    metrics["readTime"] = time.time() - start
 
+    # main algorithm
     start = time.time()
     if algorithm == 'bigclam':
         trainComm = bigClam(graph, k)
@@ -34,12 +37,13 @@ if __name__ == '__main__':
     else:
         print("invalid algorithm")
         exit(-1)
-    matrics["execTime"] = time.time()-start
+    metrics["execTime"] = time.time() - start
 
-    # evaluation matrics
-    matrics["f1score"] = Util.f1score(realComm,trainComm)
-    matrics["omgIdx"] = Util.omegaIndex(realComm,trainComm)
-    matrics["accuracy"] = Util.accuracy(realComm,trainComm)
+    # evaluation metrics
+    metrics["f1score"] = Util.f1score(realComm, trainComm)
+    metrics["omgIdx"] = Util.omegaIndex(realComm, trainComm)
+    metrics["accuracy"] = Util.accuracy(realComm, trainComm)
 
-    Util.writeMetrics(output, matrics)
+    # output figure and metrics
+    Util.writeMetrics(output, metrics)
     Util.visualize(trainComm)
