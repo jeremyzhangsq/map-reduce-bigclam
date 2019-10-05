0470b81a1f748aaaaa2e0e6ebfaa447fa5a096c0
jeremyzhangsq <jeremyzhangsq@gmail.com>
2019-10-06 00:24:54 +0800

    cleaned data

--- Util.py
+++ Util.py
@@ -1,8 +1,25 @@
+import numpy as np
+
 
 def readNetwork(filename):
-    pass
+    file = open(filename,"r")
+    n,m = file.readline().rstrip("\n").split("\t")
+    adj = np.zeros((int(n), int(n)), dtype=np.int8)
+    for i in range(int(m)):
+        s,t = file.readline().rstrip("\n").split("\t")
+        adj[int(s),int(t)] = 1
+    file.close()
+    return adj
 
 def readCommunity(filename):
+    file = open(filename,"r")
+    l = []
+    for line in file.readlines():
+        each = line.rstrip("\n").split("\t")
+        each = [int(a) for a in each]
+        l.append(each)
+
+    file.close()
     pass
 
 def visualize(community):
