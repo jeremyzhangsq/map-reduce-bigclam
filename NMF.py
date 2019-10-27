a1f0d420e6b149ba5257209e8abe0c55929a8857
unknown <421895781@qq.com>
2019-10-27 15:24:45 +0800

    update some parameters of NMF

--- NMF.py
+++ NMF.py
@@ -12,6 +12,7 @@ import numpy as np
 from numpy.linalg import norm
 from time import time
 from sys import stdout
+from numpy.linalg import lstsq
 
 
 def NMF(G, k):
@@ -20,15 +21,15 @@ def NMF(G, k):
     W,H: output solution
     Winit,Hinit: initial solution
     """
-    tol = 0.001  # tol: tolerance for a relative stopping condition
+    tol = 0.00001  # tol: tolerance for a relative stopping condition
     timelimit = 10000  # timelimit: limit of time
-    maxiter = 10000 # maxiter: limit of iterations
+    maxiter = 10000  # maxiter: limit of iterations
     V = G.matrix
     Winit = np.random.rand(np.size(V, 0), k)  # initial solution
-    Hinit = np.random.rand(k, np.size(V, 1)) # initial solution
+    Hinit = np.random.rand(k, np.size(V, 1))  # initial solution
 
     W = Winit;
-    H = Hinit; # output solution
+    H = Hinit;  # output solution
     initt = time();
 
     gradW = dot(W, dot(H, H.T)) - dot(V, H.T)
@@ -55,7 +56,6 @@ def NMF(G, k):
 
         if iter % 10 == 0: stdout.write('.')
 
-
     print('\nIter = %d Final proj-grad norm %f' % (iter, projnorm))
 
     C = {}
@@ -81,8 +81,8 @@ def nlssubprob(V, W, Hinit, tol, maxiter):
     WtV = dot(W.T, V)
     WtW = dot(W.T, W)
 
-    alpha = 1;
-    beta = 0.1;
+    alpha = 0.05;
+    beta = 0.3;
     for iter in range(1, maxiter):
         grad = dot(WtW, H) - WtV
         projgrad = norm(grad[logical_or(grad < 0, H > 0)])
@@ -116,3 +116,167 @@ def nlssubprob(V, W, Hinit, tol, maxiter):
         if iter == maxiter:
             print('Max iter in nlssubprob')
     return (H, grad, iter)
+
+
+# # Multiplicative Update Approach
+# def mu(G, k):
+#     '''
+#     Run multiplicative updates to perform nonnegative matrix factorization on A.
+#     Return matrices W, H such that A = WH.
+#
+#     Parameters:
+#         A: ndarray
+#             - m by n matrix to factorize
+#         k: int
+#             - integer specifying the column length of W / the row length of H
+#             - the resulting matrices W, H will have sizes of m by k and k by n, respectively
+#         delta: float
+#             - float that will be added to the numerators of the update rules
+#             - necessary to avoid division by zero problems
+#         num_iter: int
+#             - number of iterations for the multiplicative updates algorithm
+#         init_W: ndarray
+#             - m by k matrix for the initial W
+#         init_H: ndarray
+#             - k by n matrix for the initial H
+#         print_enabled: boolean
+#             - if ture, output print statements
+#
+#     Returns:
+#         W: ndarray
+#             - m by k matrix where k = dim
+#         H: ndarray
+#             - k by n matrix where k = dim
+#     '''
+#
+#     A = G.matrix
+#     delta = 0.0000001
+#     num_iter = 5
+#     init_W = None
+#     init_H = None
+#     print_enabled = True
+#
+#     print('Applying multiplicative updates on the input matrix...')
+#
+#     if print_enabled:
+#         print('---------------------------------------------------------------------')
+#         print('Frobenius norm ||A - WH||_F')
+#         print('')
+#
+#     # Initialize W and H
+#     if init_W is None:
+#         W = np.random.rand(np.size(A, 0), k)
+#     else:
+#         W = init_W
+#
+#     if init_H is None:
+#         H = np.random.rand(k, np.size(A, 1))
+#     else:
+#         H = init_H
+#
+#     # Decompose the input matrix
+#     for n in range(num_iter):
+#
+#         # Update H
+#         W_TA = W.T @ A
+#         W_TWH = W.T @ W @ H + delta
+#
+#         for i in range(np.size(H, 0)):
+#             for j in range(np.size(H, 1)):
+#                 H[i, j] = H[i, j] * W_TA[i, j] / W_TWH[i, j]
+#
+#         # Update W
+#         AH_T = A @ H.T
+#         WHH_T = W @ H @ H.T + delta
+#
+#         for i in range(np.size(W, 0)):
+#             for j in range(np.size(W, 1)):
+#                 W[i, j] = W[i, j] * AH_T[i, j] / WHH_T[i, j]
+#
+#         if print_enabled:
+#             frob_norm = np.linalg.norm(A - W @ H, 'fro')
+#             print("iteration " + str(n + 1) + ": " + str(frob_norm))
+#
+#     C = {}
+#     for idx, row in enumerate(W):
+#         list_row = row.tolist()
+#         max_index = list_row.index(max(list_row))  # return index of max value
+#         C.setdefault(str(max_index), []).append(str(idx))
+#
+#     return C
+#
+#
+# # Alternating Non-negative Least Squares
+# def als(G, k):
+#     '''
+#     Run the alternating least squares method to perform nonnegative matrix factorization on A.
+#     Return matrices W, H such that A = WH.
+#
+#     Parameters:
+#         A: ndarray
+#             - m by n matrix to factorize
+#         k: int
+#             - integer specifying the column length of W / the row length of H
+#             - the resulting matrices W, H will have sizes of m by k and k by n, respectively
+#         num_iter: int
+#             - number of iterations for the multiplicative updates algorithm
+#         print_enabled: boolean
+#             - if ture, output print statements
+#
+#     Returns:
+#         W: ndarray
+#             - m by k matrix where k = dim
+#         H: ndarray
+#             - k by n matrix where k = dim
+#     '''
+#
+#     A = G.matrix
+#     num_iter = 10
+#     init_W = None
+#     init_H = None
+#     print_enabled = True
+#
+#     print('Applying the alternating least squares method on the input matrix...')
+#
+#     if print_enabled:
+#         print('---------------------------------------------------------------------')
+#         print('Frobenius norm ||A - WH||_F')
+#         print('')
+#
+#     # Initialize W and H
+#     if init_W is None:
+#         W = np.random.rand(np.size(A, 0), k)
+#     else:
+#         W = init_W
+#
+#     if init_H is None:
+#         H = np.random.rand(k, np.size(A, 1))
+#     else:
+#         H = init_H
+#
+#     # Decompose the input matrix
+#     for n in range(num_iter):
+#         # Update H
+#         # Solve the least squares problem: argmin_H ||WH - A||
+#         H = lstsq(W, A, rcond=-1)[0]
+#         # Set negative elements of H to 0
+#         H[H < 0] = 0
+#
+#         # Update W
+#         # Solve the least squares problem: argmin_W.T ||H.TW.T - A.T||
+#         W = lstsq(H.T, A.T, rcond=-1)[0].T
+#
+#         # Set negative elements of W to 0
+#         W[W < 0] = 0
+#
+#         if print_enabled:
+#             frob_norm = np.linalg.norm(A - W @ H, 'fro')
+#             print("iteration " + str(n + 1) + ": " + str(frob_norm))
+#
+#     C = {}
+#     for idx, row in enumerate(W):
+#         list_row = row.tolist()
+#         max_index = list_row.index(max(list_row))  # return index of max value
+#         C.setdefault(str(max_index), []).append(str(idx))
+#
+#     return C
